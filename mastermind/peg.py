39637991f0155fe8061eab60a6c6b135e2c24b0e
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-24 18:39:20 -0700

    Initial commit, add project structure for CircleCI

---
+++ mastermind/peg.py
@@ -0,0 +1,5 @@
+class Peg(object):
+  "Value object which understands its color"
+
+  def __init__(self):
+    pass
---
+++ tests/test_peg.py
@@ -0,0 +1,6 @@
+import unittest
+
+class peg_test(unittest.TestCase):
+  "Unit Tests for Peg class value object"
+
+unittest.main()
