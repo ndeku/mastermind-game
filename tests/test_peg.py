484a78c7fe76ef54885b62435753c3d7014e6d8f
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-24 21:33:40 -0700

    Add test for string printing

--- tests/test_peg.py
+++ tests/test_peg.py
@@ -15,4 +15,15 @@ class peg_test(unittest.TestCase):
     peg_b = Peg("Green")
     self.assertEqual(peg_a, peg_b, msg=f"Two Green Pegs should be equal: {peg_a} {peg_b}")
 
+  def test_pegs_different_color_not_equal(self):
+    peg_a = Peg("Green")
+    peg_b = Peg("Blue")
+    self.assertNotEqual(peg_a, peg_b, msg=f"Two Pegs of different color should be equal: {peg_a} {peg_b}")
+
+  def test_pegs_print_correct_color(self):
+    peg_a = Peg("Green")
+    self.assertEqual("Green", str(peg_a))
+
+  
+
 unittest.main()
