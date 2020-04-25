6ddd7f90042de773fef6838cc56817c752512a02
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-24 23:51:31 -0700

    Add generate_solution method

--- mastermind/game.py
+++ mastermind/game.py
@@ -1,13 +1,20 @@
 from mastermind.peg import Peg
-#from peg import Peg
+from random import choices
 
 class Game(object):
   "Understands state of game and win/loss"
 
-  def __init__(self):
-    self.answer = [Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]
+  def __init__(self, answer=None):
+    self.k = 4 # number of classes
+    self.num_spaces = 4 # number of places/ spaces
+    self.answer = answer if answer else self.__generate_solution(answer)
 
   def make_guess(self, guess):
     if (guess == self.answer):
       return "Win!!!"
     return "Try again"
+
+  def __generate_solution(self, answer):
+    "Generates a game solution among possible classes"
+    classes = list(Peg(i) for i in range(0, self.k))
+    return choices(classes, k=self.num_spaces)
--- tests/test_game.py
+++ tests/test_game.py
@@ -7,10 +7,16 @@ class game_test(unittest.TestCase):
 
   def test_correct_answer_wins(self):
     guess = [Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]
-    self.assertEqual("Win!!!", Game().make_guess(guess))
+    game = Game(guess)
+    self.assertEqual("Win!!!", game.make_guess(guess))
 
   def test_incorrect_answer_does_not_win(self):
+    game_one = Game([Peg("Green"), Peg("Green"), Peg("Blue"), Peg("Blue")])
+    self.assertNotEqual("Win!!!", game_one.make_guess([Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]))
+
+  def test_generate_random_answer(self):
+    game_two = Game() # game without solution
     guess = [Peg("Green"), Peg("Green"), Peg("Blue"), Peg("Blue")]
-    self.assertNotEqual("Win!!!", Game().make_guess(guess))
+    self.assertNotEqual("Win!!!", game_two.make_guess(guess))
 
 unittest.main()
