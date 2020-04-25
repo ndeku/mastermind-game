41c8ef5240bb7678c3f95705fefdb1dee834ef95
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-25 01:05:45 -0700

    Add win loss logic for maximum guesses

--- mastermind/game.py
+++ mastermind/game.py
@@ -8,13 +8,23 @@ class Game(object):
     self.k = 4 # number of classes
     self.num_spaces = 4 # number of places/ spaces
     self.answer = answer if answer else self.__generate_solution()
+    self.guesses = []
 
   def make_guess(self, guess):
-    if (guess == self.answer):
+    self.guesses.append(guess)
+    if len(self.guesses) > 10:
+      return "Loss, too many guesses" #CodeMaster wins
+    if (self.__is_solution(guess)):
       return "Win!!!"
+    if len(self.guesses) == 10:
+      return "Loss, too many guesses"
     return "Try again"
+
+  def __is_solution(self, guess):
+    return (guess == self.answer)
 
   def __generate_solution(self):
     "Generates a game solution among possible classes"
-    classes = list(Peg(i) for i in range(0, self.k))
+    #classes = list(Peg(i) for i in range(0, self.k))
+    classes = [Peg("Green"), Peg("Blue"), Peg("Pink"), Peg("Yellow")]
     return choices(classes, k=self.num_spaces)
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,7 @@
 import unittest
 from mastermind.game import Game
 from mastermind.peg import Peg
+from random import choices
 
 class game_test(unittest.TestCase):
   "Unit Tests for Mastermind game class"
@@ -18,5 +19,34 @@ class game_test(unittest.TestCase):
     game_two = Game() # game without solution
     guess = [Peg("Green"), Peg("Green"), Peg("Blue"), Peg("Blue")]
     self.assertNotEqual("Win!!!", game_two.make_guess(guess))
+  
+  def test_ten_incorrect_guesses_is_loss(self):
+    game_two = Game([Peg("Green"), Peg("Blue"), Peg("Pink"), Peg("Yellow")])
+    for i in range(0, 10):
+      result = game_two.make_guess([Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")])
+    self.assertEqual("Loss, too many guesses", result)
+
+  def test_fifteen_incorrect_guesses_is_loss(self):
+    game = Game([Peg("Green"), Peg("Blue"), Peg("Pink"), Peg("Yellow")])
+    incorrect_guess = [Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]
+    for i in range(0, 15):
+      result = game.make_guess(incorrect_guess)
+    self.assertEqual("Loss, too many guesses", result)
+
+  def test_correct_answer_on_tenth_try_is_win(self):
+    solution = [Peg("Green"), Peg("Blue"), Peg("Pink"), Peg("Yellow")]
+    game = Game(solution)
+    incorrect_guess = [Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]
+    for i in range(0, 9):
+      result = game.make_guess(incorrect_guess)
+    self.assertEqual("Win!!!", game.make_guess(solution))
+
+  def test_correct_answer_fourth_try_wins(self):
+    solution = [Peg("Green"), Peg("Blue"), Peg("Pink"), Peg("Yellow")]
+    game = Game(solution)
+    incorrect_guess = [Peg("Green"), Peg("Green"), Peg("Green"), Peg("Green")]
+    for i in range(0, 3):
+      result = game.make_guess(incorrect_guess)
+    self.assertEqual("Win!!!", game.make_guess(solution))
 
 unittest.main()
