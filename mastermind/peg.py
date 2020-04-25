e8cc8495a146e9c36a9f465f123ac59749a2405a
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-24 20:36:29 -0700

    Add peg class and test peg equals itself

--- mastermind/peg.py
+++ mastermind/peg.py
@@ -1,5 +1,5 @@
 class Peg(object):
   "Value object which understands its color"
 
-  def __init__(self):
-    pass
+  def __init__(self, color):
+    self.color = color
--- tests/test_peg.py
+++ tests/test_peg.py
@@ -1,6 +1,13 @@
 import unittest
+from mastermind.peg import Peg
 
 class peg_test(unittest.TestCase):
   "Unit Tests for Peg class value object"
+
+  def test_peg_equals_itself(self):
+    peg_a = Peg("Green")
+    self.assertEqual(peg_a, peg_a, msg="Peg does not Equal itself")
+    peg_a = Peg(1)
+    self.assertEqual(peg_a, peg_a, msg="Peg does not Equal itself")
 
 unittest.main()
