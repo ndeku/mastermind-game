56682d012bd47f338e9fb4995f4f8f35d1c70260
Nicholas Dekutoski <nick_dekutoski@berkeley.edu>
2020-04-26 11:14:04 -0700

    map object to number for chi squared test

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -3,18 +3,22 @@ from mastermind.game import Game
 from mastermind.peg import Peg
 from mastermind.solver import RandomSolver
 from scipy.stats import chisquare
+import random
 
 class solver_test(unittest.TestCase):
   "Unit Tests for Mastermind game class"
 
   def test_random_solver(self):
+    seed = random.seed(42) 
     game = Game()
     solver = RandomSolver(game)
     guesses = []
     for i in range(0, 50):
       guesses.append(solver.generate_guess())
-    guesses = [hash(g) for guess_list in guesses for g in guess_list]
+    guesses = [str(g) for guess_list in guesses for g in guess_list]
+    guess_map = {item: idx for idx, item in enumerate(set(guesses))}
+    guesses = [guess_map.get(g) for g in guesses]
     t_stat, p_val = chisquare(guesses)
-    self.assertGreater(p_val, .1)
+    self.assertGreater(p_val, 0.05, msg=f"p-value should should be < .05, pval={p_val}")
 
 unittest.main()
